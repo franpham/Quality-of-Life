ce3a723d42313abc6cbba743e79eced913d409aa
Kasi Onaga <kasi@Kasis-MacBook-Air.local>
2015-11-16 16:47:26 -1000

    Finished matched_cityCodes.js

--- app/db/associatedKeywordsList.js
+++
@@ -1,8 +0,0 @@
-associatedKeywordsList = [
-
-  "banana",
-  "sdfjsdf",
-  "skldjflksjdfksjdf",
-  "Nigel said... "
-
-]
---
+++ routes/matched_cityCodes.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var matchedCities = [
+  [
+    'Los Angeles',
+    'San Jose-Sunnyvale-Santa Clara, CA',
+     '00002'
+  ],[
+    'San Jose',
+    'San Jose-Sunnyvale-Santa Clara',
+    '00008'
+  ],[
+    'San Francisco',
+    'San Francisco-San Mateo-Redwood City',
+    '00010'
+  ],[
+    'Washington',
+    'Washington-Arlington-Alexadria',
+    '00018'
+  ],[
+    'Boston',
+    'Boston-Cambridge-Quincy',
+    '00016'
+  ],[
+    'Miami',
+    'Miami-Miami Beach-Kendall',
+    '00033'
+  ],[
+    'New Orleans',
+    'New Orleans-Metairie-Kenner',
+    '09993'
+  ]
+ ];
