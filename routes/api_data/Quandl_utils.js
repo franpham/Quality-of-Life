1ee63e88f60f0f9719bda35b3e0f77b524c4e4ae
Francis Pham <franpham@gmail.com>
2015-11-20 15:17:09 -1000

    enabled API to get all cities data for homes_stats & jobs_list with one request

--- routes/api_data/Quandl_utils.js
+++ routes/api_data/Quandl_utils.js
@@ -1,5 +1,56 @@
 "use strict";
 
+var codeSet = ['00003',  // Los Angeles is default city for CA;
+              '00009',  // Miami is default city for FL;
+              '00047', '00015', '00046',  // default cities for LA, AZ, CT;
+              '00011', '00010', '00022', '00035', '00043', '00050',   // codes for states with 1 city;
+              '00054', '00088', '00075', '00590', '00008', '00004',
+              '00013', '00002', '00006', '00005', '00016', '00473'];
+
+var states = [
+  { 'CA' : { '-1' : ['00018', '00014', '00044', '00012', '00032', '00037', '00025', '00064', '00056', '00003'] }}, // 10
+  { 'FL' : { '-2' : ['00020', '00027', '00009', '00128', '00041', '00254'] }}, // 6
+  { 'LA' : { '-3' : ['00066', '00047'] }},
+  { 'AZ' : { '-4' : ['00015', '00053'] }},
+  { 'CT' : { '-5' : ['00057', '00046'] }},
+  { 'MA' : '00011' },
+  { 'GA' : '00010' },
+  { 'CO' : '00022' },
+  { 'IN' : '00035' },
+  { 'KY' : '00043' },
+  { 'AL' : '00050' },
+  { 'HI' : '00054' },
+  { 'IA' : '00088' },
+  { 'AR' : '00075' },
+  { 'KS' : '00590' },
+  { 'DC' : '00008' },
+  { 'IL' : '00004' },
+  { 'MI' : '00013' },
+  { 'NY' : '00002' },
+  { 'PA' : '00006' },
+  { 'TX' : '00005' },
+  { 'WA' : '00016' },
+  { 'DE' : '00473' }
+];
+var MULTISTATES = 5;
+
+// client side usage:
+// var quandl = require('./api_data/Quandl_utils.js');
+// var datatype; // should be 'careers', 'homes', or 'rents'
+// var jobCat;   // should be job category from Indeed_jobCats.js
+// if (oldCode !== newCode && quandl.isMultiCityState(newCode)) {
+//    var json = $.getJson('http://localhost/' + datatype + '/' + newCode + (datatype === 'careers' ? '/' + encodeURI(jobCat) : ''));
+      // get the data from the json object to update the color of the map;
+// }
+function isMultiCityState(cityCode) {
+  for (var i = 0; i < MULTISTATES; i++) {
+    var st = states[i];
+    if (st.indexOf(cityCode) >= 0)
+      return true;
+  }
+  return false;
+}
+
 var data = [
   {'Atlanta, GA' : '00010'},
   {'Phoenix, AZ' : '00015'},
@@ -23,14 +74,14 @@ var data = [
   {'New Orleans, LA' : '00047'},
   {'Birmingham, AL' : '00050'},
   {'Honolulu, HI' : '00054'},
-  {'Springfield, MA' : '00077'},    // 2ND MA
+  {'Wilmington, DE' : '00473'},     // city code
   {'Baton Rouge, LA' : '00066'},    // 2ND LA
   {'Tucson, AZ' : '00053'},         // 2ND AZ
   {'Stamford, CT' : '00057'},       // 2ND CT
   {'Des Moines, IA' : '00088'},
   {'Little Rock, AR' : '00075'},
   {'Fresno, CA' : '00056'},
-  {'Bakersfield, CA' : '00063'},
+  {'West Palm Beach' : '00254'},    // city code
   {'Wichita, KS' : '00590'},
   {'Los Angeles, CA' : '00003'},
   {'Washington, DC' : '00008'},
@@ -63,8 +114,8 @@ function getCity(cityCode) {
 }
 
 function getQuandlCode(cityCode) {
-  var code = parseInt(cityCode);                    // C = city, M = metro;
-  return (code ===  37 || code === 44 || code === 128) ? 'C' + cityCode : 'M' + cityCode;
+  var code = parseInt(cityCode);                      // C = city, M = metro;
+  return (code == 37 || code == 44 || code == 128 || code == 254 || code == 473) ? 'C' + cityCode : 'M' + cityCode;
 }
 
 function parseRents(json) {
@@ -78,4 +129,5 @@ function parseRents(json) {
   return rents;
 }
 
-module.exports = { cities: cities, codes: codes, getState: getState, getCity: getCity, getQuandlCode: getQuandlCode, parseRents: parseRents };
+module.exports = { states: states, cities: cities, codes: codes, codeSet: codeSet, getState: getState, getCity: getCity,
+               getQuandlCode: getQuandlCode, parseRents: parseRents, isMultiCityState: isMultiCityState };
